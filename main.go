3958ddb7f31cff20456613b3c845a5132e158d9c
NEBULA1978 <rpascualey@gmail.com>
2023-03-31 15:18:00 +0200

    Añadiendo opciones

--- main.go
+++ main.go
@@ -9,23 +9,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var rootCmd = &cobra.Command{
-	Use:   "verdades",
-	Short: "Un juego de verdades en Go",
-	Long:  `Un juego simple de verdades para jugar con amigos.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		rand.Seed(time.Now().Unix())
-		fmt.Println("Bienvenido a Verdades en Go!")
-		fmt.Println("¿Listo para jugar?")
-		fmt.Println("¡Comencemos!")
-		fmt.Println("¿Qué quieres hacer?")
-
-		opciones := []string{"Verdad", "Desafío"}
-		opcion := opciones[rand.Intn(len(opciones))]
-		fmt.Printf("%s\n", opcion)
-	},
-}
-
 var langCmd = &cobra.Command{
 	Use:   "lang",
 	Short: "Cambiar el idioma del juego",
@@ -53,6 +36,35 @@ var duracionCmd = &cobra.Command{
 	},
 }
 
+var rootCmd = &cobra.Command{
+	Use:   "verdades",
+	Short: "Un juego de verdades en Go",
+	Long:  `Un juego simple de verdades para jugar con amigos.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		rand.Seed(time.Now().Unix())
+		fmt.Println("Bienvenido a Verdades en Go!")
+		fmt.Println("¿Listo para jugar?")
+		fmt.Println("¡Comencemos!")
+
+		for {
+			opciones := []string{"Verdad", "Desafío"}
+			opcion := opciones[rand.Intn(len(opciones))]
+			fmt.Printf("%s\n", opcion)
+
+			// Espera a que el usuario presione Enter para continuar
+			fmt.Scanln()
+
+			// Pregunta al usuario si quiere jugar de nuevo
+			fmt.Println("¿Quieres jugar de nuevo? (S/N)")
+			var respuesta string
+			fmt.Scanln(&respuesta)
+			if respuesta != "S" && respuesta != "s" {
+				break
+			}
+		}
+	},
+}
+
 func init() {
 	rootCmd.PersistentFlags().String("lang", "es", "idioma del juego (es/en)")
 
@@ -70,10 +82,26 @@ func main() {
 	}
 }
 
-// Resultado por consola
+// PARA EJECUTAR POR CONSOLA
 
-// next@next-System:~/Vídeos/GO/app-go-IA$ ./mi-juego lang --lang en
-// Language changed to English.
-// next@next-System:~/Vídeos/GO/app-go-IA$ ./mi-juego duracion --duracion 15
-// La duración del juego es de 15 minutos.
-// next@next-System:~/Vídeos/GO/app-go-IA$
+// Para ejecutar el juego, desde la línea de comandos, debes ubicarte en el directorio donde se encuentra el archivo main.go y luego ejecutar el siguiente comando:
+
+// go
+
+// go run main.go
+
+// Esto iniciará el juego y mostrará en pantalla las opciones disponibles para cambiar el idioma del juego o la duración del mismo.
+
+// Para cambiar el idioma del juego a inglés, debes ejecutar el siguiente comando:
+
+// bash
+
+// ./mi-juego lang --lang en
+
+// Para configurar la duración del juego a 15 minutos, debes ejecutar el siguiente comando:
+
+// bash
+
+// ./mi-juego duracion --duracion 15
+
+// Recuerda que para que los cambios de idioma o duración del juego tengan efecto, debes reiniciar el juego.
